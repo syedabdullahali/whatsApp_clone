d4573ac83ca8570c176afc37a44f5fa61899cd5d
syedabdullahali <syedabdullah380@gmail.com>
2023-01-02 12:22:37 +0530

    initial file and  folder updated 🚧

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Let's build whats app</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCFCt0N8qFq9Jhn9ww0r4k_3pk-I6tgqgk",
+    authDomain: "whatsapp-clone-b9078.firebaseapp.com",
+    projectId: "whatsapp-clone-b9078",
+    storageBucket: "whatsapp-clone-b9078.appspot.com",
+    messagingSenderId: "1059248953108",
+    appId: "1:1059248953108:web:db0be6bef05475f5e29401",
+    measurementId: "G-DSN7WY17RT"
+  };
