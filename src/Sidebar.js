338f7fce05ba23680032f7f643712790da591583
syedabdullahali <syedabdullah380@gmail.com>
2023-01-03 15:53:04 +0530

    added new components 🍱

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,16 +1,50 @@
 import React from 'react'
 import './Sidebar.css'
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import DonutLargeIcon from '@mui/icons-material/DonutLarge';
+import ChatIcon from '@mui/icons-material/Chat';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import SearchIcon from '@mui/icons-material/Search';
+import { IconButton } from '@mui/material';
+import SidebarChat from './SidebarChat';
 
 const Sidebar = () => {
   return (
     <div className='sidebar'>
        <div className="sidebar__header">
+        <IconButton>    
+                <AccountCircleIcon className='account_icon'/>
+        </IconButton>
+        <div className="sidebar__headerRight">
+        <IconButton>    
+       <DonutLargeIcon/>
+       </IconButton>
 
-       </div>
+       <IconButton>    
+       <ChatIcon/>
+       </IconButton>
+
+       <IconButton>    
+       <MoreVertIcon/>
+       </IconButton>
+        </div>
+       </div> 
        <div className="sidebar_search">
-        
+          <div className="sidebar__searchContainer">
+             <SearchIcon/>
+             <input type="text" placeholder='Search or start new chat' />
+          </div>
        </div>
-    </div>
+         
+         <div className="sidebar__chat">
+
+               <SidebarChat/>
+               <SidebarChat/>
+               <SidebarChat/>
+               <SidebarChat/>
+            
+         </div>       
+      </div>
   )
 }
 
---
+++ src/SidebarChat.js
@@ -0,0 +1,20 @@
+import './SidebarChat.css'
+
+const SidebarChat = () => {
+  return (
+    <div className='sidebarChat'>
+        
+      <div className='contact'>
+              
+              
+      </div>
+
+      <div className="sidebarChat__info">
+        <h2>Room name</h2>
+        <p>Last Message.....</p>
+      </div>
+    </div>
+  )
+}
+
+export default SidebarChat
